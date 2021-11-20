ffe15e5ef055619f91d64322f5d31eeb422c39b6
maurovio <mcvio78@gmail.com>
2021-11-20 20:43:36 +0100

    style: add styled-normalize

--- src/hoc/Global.tsx
+++ src/hoc/Global.tsx
@@ -1,9 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
 import { ReactNode } from 'react';
 
 const GlobalStyle = createGlobalStyle`
+  ${normalize}
+
   body {
-    margin: 0;
     font-family:
       -apple-system,
       BlinkMacSystemFont,

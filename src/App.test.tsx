9bc53705399917107b013432584c5fd1821dff7c
maurovio <mcvio78@gmail.com>
2021-11-21 23:35:07 +0100

    test: adapt the individual test to the basic components

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react';
 
-import App from './App';
+import { App } from './App';
+import { Theme } from './hoc/Theme';
 
-test('renders learn react link', () => {
-  render(<App />);
+test('renders my habits list', () => {
+  render(
+    <Theme>
+      <App />
+    </Theme>,
+  );
   const linkElement = screen.getByText(/my habits list/i);
   expect(linkElement).toBeInTheDocument();
 });

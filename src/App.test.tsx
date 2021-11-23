ebac143ed0428233e83731751b8d458d4d0fd3ef
maurovio <mcvio78@gmail.com>
2021-11-23 23:31:58 +0100

    test: modify and refactor App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,9 @@
 import { render, screen } from '@testing-library/react';
 
 import { App } from './App';
-import { Theme } from './hoc/Theme';
 
 test('renders my habits list', () => {
-  render(
-    <Theme>
-      <App />
-    </Theme>,
-  );
+  render(<App />);
   const linkElement = screen.getByText(/my habits list/i);
   expect(linkElement).toBeInTheDocument();
 });

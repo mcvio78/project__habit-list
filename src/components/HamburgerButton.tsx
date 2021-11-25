2e968691ff7c9e4d86b5264bb2bbe5fe27a9e4ee
maurovio <mcvio78@gmail.com>
2021-11-25 23:39:40 +0100

    build: create HamburgerButton component

---
+++ src/components/HamburgerButton.tsx
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+import React, { useState } from 'react';
+
+interface ButtonContainerProps {
+  open: boolean;
+}
+
+const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 40px;
+  height: 26px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: transparent;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+
+  :focus {
+    outline: rgba(var(--shadow-text-rgb), 0.1) 2px solid;
+  }
+
+  div {
+    width: 100%;
+    height: 4px;
+    background-color: var(--colors-text);
+    box-shadow: ${({ open }) =>
+      !open && `0 4px 4px rgba(var(--shadow-text-rgb), 0.25)`};
+    transition: all 1s linear;
+
+    :first-child {
+      transform: ${({ open }) =>
+        open ? 'translateY(11px) rotate(45deg) ' : 'rotate(0) translateY(0)'};
+    }
+
+    :nth-child(2) {
+      opacity: ${({ open }) => (open ? '0' : '1')};
+      transform: ${({ open }) => (open ? 'translateX(20px)' : 'translateX(0)')};
+    }
+
+    :nth-child(3) {
+      transform: ${({ open }) =>
+        open ? 'translateY(-11px) rotate(-45deg) ' : 'rotate(0) translateY(0)'};
+    }
+  }
+`;
+
+export const HamburgerButton = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <ButtonContainer open={isOpen} onClick={() => setIsOpen(!isOpen)}>
+      <div />
+      <div />
+      <div />
+    </ButtonContainer>
+  );
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,21 +2,33 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { PageLayout } from '../components/layout/PageLayout';
-import { Display1, ParagraphLarge } from '../components/Typograpy';
+import { Display1, ParagraphLarge } from '../components/Typography';
+import { HamburgerButton } from '../components/HamburgerButton';
 
 export const Home = () => {
   const HomeLayout = styled(PageLayout)`
     > :first-child {
       align-self: flex-start;
-      margin-left: 20px;
     }
+  `;
+
+  const Container = styled.div`
+    width: 100%;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 28px;
+    padding: 0 28px;
   `;
 
   return (
     <HomeLayout>
-      <ParagraphLarge italic shadow>
-        *not backed up
-      </ParagraphLarge>
+      <Container>
+        <ParagraphLarge italic shadow>
+          *not backed up
+        </ParagraphLarge>
+        <HamburgerButton />
+      </Container>
       <Display1 shadow>
         My
         <br />

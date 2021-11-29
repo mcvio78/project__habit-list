4b742cf6f87e6f442bdb7ea63b362481bb09bfcf
maurovio <mcvio78@gmail.com>
2021-11-29 23:51:40 +0100

    refactor: remove layout (local) and import new components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,39 +1,37 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-
 import { PageLayout } from '../components/layout/PageLayout';
+import { Container } from '../components/layout/Container';
 import { Display1, ParagraphLarge } from '../components/Typography';
 import { HamburgerButton } from '../components/buttons/HamburgerButton';
-
-const HomeLayout = styled(PageLayout)`
-  > :first-child {
-    align-self: flex-start;
-  }
-`;
-
-const Container = styled.div`
-  width: 100%;
-  box-sizing: border-box;
-  display: flex;
-  justify-content: space-between;
-  margin-top: 28px;
-  padding: 0 28px;
-`;
+import { BigButton } from '../components/buttons/BigButton';
 
 export const Home = () => {
   return (
-    <HomeLayout>
-      <Container>
-        <ParagraphLarge italic shadow>
+    <PageLayout>
+      <Container
+        w="100%"
+        bs="border-box"
+        jc="space-between"
+        mt="28px"
+        p="0 28px"
+      >
+        <ParagraphLarge italic shadowText>
           *not backed up
         </ParagraphLarge>
-        <HamburgerButton />
+        <HamburgerButton shadowBox />
       </Container>
-      <Display1 shadow>
+      <Display1 shadowText>
         My
         <br />
         Habits List
       </Display1>
-    </HomeLayout>
+      <Container fd="column" mt="auto" mb="30px" h="160px" jc="space-between">
+        <BigButton italic shadowBox shadowText>
+          Create Habits
+        </BigButton>
+        <BigButton italic shadowBox shadowText>
+          Quick Start
+        </BigButton>
+      </Container>
+    </PageLayout>
   );
 };

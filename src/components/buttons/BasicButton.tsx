b232acfa87fc384c68c480f8aea26fdaa8bf0b8d
maurovio <mcvio78@gmail.com>
2021-11-29 23:27:10 +0100

    build: create BasicButton component

--- src/components/buttons/BasicButton.tsx
+++ src/components/buttons/BasicButton.tsx
@@ -1,22 +1,30 @@
 import styled from 'styled-components/macro';
 
 export interface BasicButtonProps {
-  type?: string;
   title: string;
+  id?: string;
+  type?: 'button' | 'submit' | 'reset' | undefined;
+  value?: string | number;
+  ariaLabel?: string;
+  ariaLabelledBy?: string;
   bgColor?: string;
+  shadowBox?: boolean;
 }
 
 export const BasicButton = styled.button.attrs(props => ({
-  type: props.type || 'button',
   title: props.title,
+  type: props.type || 'button',
 }))<BasicButtonProps>`
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
-  background-color: ${props => props.bgColor || 'transparent'};
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ bgColor }) => bgColor || 'transparent'};
   border: none;
   padding: 0;
   font: inherit;
   cursor: pointer;
   outline: inherit;
+  box-shadow: ${({ shadowBox }) =>
+    shadowBox ? ` 0 4px 4px rgba(var(--clr-nt-v02-rgb), 0.25)` : 'none'};
 `;

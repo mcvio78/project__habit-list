f85633115457941d5ffabc49c1bff7e5cf580fda
maurovio <mcvio78@gmail.com>
2021-12-22 21:28:33 +0100

    refactor: create global import for layout components

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,3 +1,5 @@
 export { PageLayout } from './PageLayout';
 export { PageContainer } from './PageContainer';
+export type { ContainerProps } from './Container';
 export { Container } from './Container';
+export { DynamicWrapper } from './DynamicWrapper';

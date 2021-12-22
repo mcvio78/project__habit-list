65d68c90eb17726b055a5b43845daba3680cf402
maurovio <mcvio78@gmail.com>
2021-12-22 21:31:21 +0100

    test: update Storybook tests

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Button } from '../components/buttons/Button';
+import { Button } from '../components/UI/buttons';
 
 export default {
   title: 'Button',

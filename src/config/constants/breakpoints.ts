eaee75bb5a618e766beba7fec5658627899dd297
maurovio <mcvio78@gmail.com>
2022-01-17 19:26:32 +0100

    refactor: define breakpoints only inside scss and export it

--- src/config/constants/breakpoints.ts
+++ src/config/constants/breakpoints.ts
@@ -1,14 +1,3 @@
-export const BREAKPOINTS = {
-  de: '0',
-  // default
-  xs: '320px',
-  // mobiles
-  sm: '480px',
-  // iPads & tablets
-  md: '768px',
-  // laptops
-  lg: '1024px',
-  // desktops
-  xl: '1200px',
-  // extra large screens & TV
-};
+import breakpoints from '../../theme/breakpoints.module.scss';
+
+export const BREAKPOINTS = breakpoints;

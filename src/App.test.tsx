a892aa61e663f7133c382d55e0dcba334f974298
maurovio <mcvio78@gmail.com>
2022-03-03 23:40:10 +0100

    test: App test refactoring

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,12 +2,15 @@ import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App';
+import { ContextProvider } from './auth/contextProvider';
 
 describe('App component tests', () => {
   test('renders paragraph text', () => {
     render(
       <BrowserRouter>
-        <App />
+        <ContextProvider>
+          <App />
+        </ContextProvider>
       </BrowserRouter>,
     );
     expect(screen.getByText('Start organizing your life!')).toBeVisible();
